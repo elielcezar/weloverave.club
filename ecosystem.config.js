bed7cfe0c67701e92c1133ac4f29fd8244a04832
Eliel Cezar <elielcezar@gmail.com>
2025-11-20 16:16:04 -0300

    ajuste standalone no ecosystem.congig.js - pt3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,15 @@
 module.exports = {
     apps: [{
       name: 'nextjs-app',
-      script: '.next/standalone/server.js', // Usa o servidor standalone gerado
-      cwd: process.cwd(),
+      script: '.next/standalone/server.js',
+      cwd: process.cwd(), // Garante que roda da raiz do projeto
       instances: 1,
       exec_mode: 'fork',
       env: {
         NODE_ENV: 'production',
-        PORT: process.env.PORT || 3000  // Mudado para 3000
+        PORT: process.env.PORT || 3000,
+        // Garantir que o Next.js encontre os arquivos estáticos
+        NEXT_STATIC_FOLDER: '.next/static'
       },
       error_file: './logs/err.log',
       out_file: './logs/out.log',
